ff49b7bc795bd12c80f162f30f6707eadf4624be
Promise Obioma <proekene16@gmail.com>
2024-02-28 08:42:48 +0100

    randomlization

--- first trial/src/FoodPositive.jsx
+++ first trial/src/FoodPositive.jsx
@@ -1,5 +1,8 @@
 
-import { useState, useCallback, useEffect } from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
+import { KeyboardBackspace } from "@mui/icons-material";
+
 
 export default function FoodPositive() {
     const [date, setDate] = useState('')
@@ -9,31 +12,59 @@ export default function FoodPositive() {
         const currentDate = new Date().toLocaleDateString('en-Uk', options)
         setDate(currentDate)
     },[])
-    
-    const [foodList, setFoodList] = useState([])
-    const fetchData = useCallback(()=>{
-        fetch('./db.json')
-        .then(response=>response.json())
+    const [randomFood, setRandomFood] = useState(null)
+    // const [foodList, setFoodList] = useState(null);
+    const fetchUrl = 'http://localhost:3500/Food'
+    useEffect(()=>{
+        fetch(fetchUrl)
+        .then(response=>{
+            return response.json()
+        })
         .then(data=>{
-            setFoodList(data)
+            console.log(`the data is ${JSON.stringify(data)}`); 
+            // setFoodList(data)})
+            const randomIndex = Math.floor(Math.random() * data.length)
+            setRandomFood(data[randomIndex]);
+        // .catch(err=>console.log(err))
         })
     },[])
-    
-    useEffect(()=>{
-        fetchData()
-    },[fetchData])
     
 
   return (
     <div className="bg-cyan-700 h-screen flex items-center justify-center">
       <div className="h-4/6 w-6/12 smm:w-10/12   justify-center items-center flex flex-col bg-creamyWhite rounded-xl relative" >
+      <Link to='/' className="absolute top-0 left-0">
+            <div className="flex items-center p-4">
+                <span><KeyboardBackspace /></span>
+                <span>Go back</span>
+            </div>
+     </Link>
         <h1>
-            Here is a Meal schedule for <span className="font-bold">{date}</span>
+            Here is a Meal schedule for <span className="font-bold">{date}:</span>
         </h1>
-        <div>
-            {
+         <div>
+               
+                { 
+                randomFood && 
+                    (
 
-            }
+                    
+                        <div >
+                            <p>
+                                your morning meal is {randomFood.Morning}
+                            </p>
+                            <p>
+                                your afternoon meal is {randomFood.Afternoon}
+                            </p>
+                            <p>
+                                your evening meal is {randomFood.Evening}
+                            </p>
+
+                        </div>
+
+                    )
+                    
+                    }
         </div>
       </div>
     </div>
